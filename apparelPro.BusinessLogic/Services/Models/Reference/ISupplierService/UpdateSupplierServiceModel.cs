c980d83b120865f78fbd43dd2de83a129f558f07
Thusith Kathaluwage <tpk0106@yahoo.com>
2026-06-07 21:39:16 +1000

    fix ef bugsr

---
+++ apparelPro.BusinessLogic/Services/Models/Reference/ISupplierService/UpdateSupplierServiceModel.cs
@@ -0,0 +1,12 @@
+﻿namespace apparelPro.BusinessLogic.Services.Models.Reference.ISupplierService
+{
+    public class UpdateSupplierServiceModel
+    {
+        public int SupplierCode { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? TelephoneNos { get; set; }
+        public string? MobileNos { get; set; }
+        public string? Fax { get; set; }
+        public Guid? AddressId { get; set; } = Guid.Empty;
+    }
+}
